1d84fd0c9db1c8298388eba457c4b6d1ecd9a91f
SuperCoon <56171741+SuperCoon666@users.noreply.github.com>
2020-04-25 03:17:01 +0300

    Add files via upload

---
+++ Арканоид/Орканоид/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace Орканоид
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.matilda1 = new MatildaWinLib.Matilda();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.стартToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.настройкаToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // matilda1
+            // 
+            this.matilda1.Control = this;
+            this.matilda1.OnConnection += new MatildaWinLib.DelegateSuperImageConnection(this.matilda1_OnConnection);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // стартToolStripMenuItem
+            // 
+            this.стартToolStripMenuItem.Name = "стартToolStripMenuItem";
+            this.стартToolStripMenuItem.Size = new System.Drawing.Size(50, 20);
+            this.стартToolStripMenuItem.Text = "Старт";
+            this.стартToolStripMenuItem.Click += new System.EventHandler(this.стартToolStripMenuItem_Click);
+            // 
+            // настройкаToolStripMenuItem
+            // 
+            this.настройкаToolStripMenuItem.Name = "настройкаToolStripMenuItem";
+            this.настройкаToolStripMenuItem.Size = new System.Drawing.Size(78, 20);
+            this.настройкаToolStripMenuItem.Text = "Настройка";
+            this.настройкаToolStripMenuItem.Click += new System.EventHandler(this.настройкаToolStripMenuItem_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.стартToolStripMenuItem,
+            this.настройкаToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1217, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.Color.Maroon;
+            this.label1.Location = new System.Drawing.Point(602, 409);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 55);
+            this.label1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(96, 128);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(93, 33);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "label2";
+            this.label2.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1217, 543);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.DoubleBuffered = true;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick);
+            this.MouseEnter += new System.EventHandler(this.Form1_MouseEnter);
+            this.MouseLeave += new System.EventHandler(this.Form1_MouseLeave);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private MatildaWinLib.Matilda matilda1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem стартToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem настройкаToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Арканоид/Орканоид/Form1.cs
@@ -0,0 +1,373 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MatildaWinLib;
+using System.IO;
+
+namespace Орканоид
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        SuperImage desk; //доска
+        SuperImage brick; // кирпич
+        int ball_count = 1; //количество шаров
+        public List<SuperBall> balls = new List<SuperBall>();
+        List<SuperImage> plus = new List<SuperImage>();
+        public int bullet_count; //кол-во пуль
+        List<SuperImage> bullets = new List<SuperImage>(); //массив пуль
+        int drop = 0; //кол-во шаров, которые упали. Нужно для вывода в файл      
+        bool color = false; //переменная для замены цвета доски
+        int breek=48; // колво кирпичей
+
+        void PaintDesk(SuperImage s, SuperImagePainter b)
+        {
+            if (color == true)
+            {
+                b.FillRectangle(0, 0, s.Width, s.Height, Color.Red);
+            }
+            else
+            {
+                b.FillRectangle(0, 0, s.Width, s.Height, Color.LightPink); 
+            }
+        }
+
+        private void Brick_OnPaint(SuperImage s, SuperImagePainter p)
+        {
+            p.FillRectangle(0, 0, s.Width, s.Height, Color.LightGoldenrodYellow);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+        
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            for (int i = 0; i < balls.Count; i++)
+            {
+                balls[i].Top += balls[i].topstep;
+                balls[i].Left += balls[i].leftstep;
+
+                if (balls[i].Top <= 0)
+                {
+                    balls[i].topstep *= -1;
+                }
+                if (balls[i].Left + balls[i].Width >= ClientRectangle.Width)
+                {
+                    balls[i].leftstep *= -1;
+                }
+                if (balls[i].Left <= 0)
+                {
+                    balls[i].leftstep *= -1;
+                }
+                if (balls[i].Top >= ClientRectangle.Height)
+                {
+                    drop++;
+                    //Directory.CreateDirectory(@"E:\Группа1\Test"); 
+                    //File.AppendAllText(@"E:\Группа1\Test\aaa.txt", "\nGame over" + drop.ToString());
+                    //File.WriteAllText(@"E:\Группа1\Test\aaa2.txt", "Game over");
+                    //File.WriteAllText(@"E:\Группа1\Test\aaa3.txt", "Game over");
+
+                    balls[i].Dispose();
+                    balls.RemoveAt(i);
+
+                    if (drop == ball_count)
+                    {
+                        
+                        
+                         label1.Text = "Game Over";
+                        timer1.Stop();
+                        matilda1.ClearAllImages();
+                        timer1.Stop();
+                    }
+                }
+            }
+
+            foreach (SuperImage f in plus)
+            {
+                f.Top += 5;
+            }
+            
+            //Вот этого цикла у тебя не было. Он нужен для того, чтобы двигать пули
+            foreach (SuperImage b in bullets)
+            {
+                b.Top -= 15;
+            }
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            //Таким образом запрещаем доске вылазить за правую границу экрана
+            if (desk != null)
+            {
+                desk.Left = Math.Min(e.X, ClientRectangle.Width - desk.Width);
+            }
+        }
+
+        private void matilda1_OnConnection(SuperImage a, SuperImage b)
+        {
+           if (a.Name == "desk" && b.Name == "ball")
+            {
+                //if (b.Top + 60 > a.Top)
+                //{
+                //    ((SuperBall)b).leftstep *= -1;
+                //}
+                //else
+                //{
+                //    ((SuperBall)b).topstep *= -1;
+                //}
+
+
+                //Здесь решается задача отталкивания метеорита от доски в правильном направлении
+                //Сам алгоритм оценки координат немного сложноват
+                //Он зашит внутри метода VerticalConnection из matils'ы
+                SuperBall ball;
+                ball = b as SuperBall;
+
+                bool x = matilda1.IsVerticalConnection(ball, a);
+                if (x == true) //Функция возвращает "правду", если метеорит ударился о верхнюю или нижнюю границы доски
+                {
+                    ball.topstep *= -1; //В этом случае "отталкиваем" метеорит вверх/вниз
+                }
+                else //Функция возвращает "ложь", если метеорит ударился о правую или левую границы доски
+                {
+                    ball.leftstep *= -1; //В этом случае "отталкиваем" метеорит вправо/влево
+                }
+            }
+            if (b.Name == "desk" && a.Name == "ball")
+            {
+                //if (a.Top + 60 > b.Top)
+                //{
+                //    ((SuperBall)a).leftstep *= -1;
+                //}
+                //else
+                //{
+                //    ((SuperBall)a).topstep *= -1;
+                //}
+
+                //Смотри зеркальное условие
+                SuperBall ball;
+                ball = a as SuperBall;
+
+                bool x = matilda1.IsVerticalConnection(ball, b);
+                if (x == true)
+                {
+                    ball.topstep *= -1;
+                }
+                else
+                {
+                    ball.leftstep *= -1;
+                }
+            }
+
+            if (a.Name == "rock" && b.Name == "ball")
+            {
+                //((SuperBall)b).topstep *= -1;
+
+                SuperBall ball;
+                ball = b as SuperBall;
+                breek--;
+                label2.Text = breek.ToString();
+
+                //Смотри столкновение метеорита с доской
+                bool x = matilda1.IsVerticalConnection(ball, a);
+                if (x == true)
+                {
+                    ball.topstep *= -1;
+                }
+                else
+                {
+                    ball.leftstep *= -1;
+                }
+                a.Dispose();
+
+                int ver = matilda1.RandomInt(1, 4);
+                if (ver == 1)
+                {
+                    SuperImage bonus = matilda1.CreateSuperImage("4_ball.png", b.Left, b.Top, 30, 30, "plus");
+                    plus.Add(bonus);
+                }
+                if (breek <= 0)
+                {
+                    label1.Text = "You Win";
+                    timer1.Stop();
+                    matilda1.ClearAllImages();
+                    timer1.Stop();
+                    matilda1.ClearAllImages();
+
+                }
+
+            }
+            if (b.Name == "rock" && a.Name == "ball")
+            {
+                //((SuperBall)a).topstep *= -1;
+
+                SuperBall ball;
+                ball = a as SuperBall;
+                breek--;
+                label2.Text = breek.ToString();
+                //Смотри столкновение метеорита с доской
+                bool x = matilda1.IsVerticalConnection(ball, b);
+                if (x == true)
+                {
+                    ball.topstep *= -1;
+                }
+                else
+                {
+                    ball.leftstep *= -1;
+                }
+                b.Dispose();
+
+                int ver = matilda1.RandomInt(1, 4);
+                if (ver == 1)
+                {
+                    SuperImage bonus = matilda1.CreateSuperImage("4_ball.png", b.Left, b.Top, 30, 30, "plus");
+                    plus.Add(bonus);
+                }
+                if (breek <= 0)
+                {
+                    label1.Text = "You Win";
+                    timer1.Stop();
+                    matilda1.ClearAllImages();
+                    timer1.Stop();
+                    matilda1.ClearAllImages();
+
+                }
+            }
+
+            if (a.Name == "plus" && b.Name == "desk")
+            {
+                a.Dispose();
+                plus.Remove(a); //Тут ты забыл удалить бонус из массива. Из-за этого он так и остаётся в "мозгах" компьютера. Цикл будет продолжать двигать невидимый шарик вниз.
+                color = true;
+                bullet_count += 5;
+            }
+            if (b.Name == "plus" && a.Name == "desk")
+            {
+                b.Dispose();
+                plus.Remove(b); //То же самое, что и в предыдущем "зеркальном" условии
+                color = true;
+                bullet_count += 5;
+            }
+
+            //Кирпичи не сбивались пулями, потому что вообще не было этого куска кода, который за это отвечает
+            if (a.Name == "rock" && b.Name == "fire!")
+            {
+                a.Dispose();
+                b.Dispose();
+                bullets.Remove(b);
+                breek--;
+                label2.Text = breek.ToString();
+                if (drop == ball_count)
+                {
+
+
+                    label1.Text = "Game Over";
+                    timer1.Stop();
+                    matilda1.ClearAllImages();
+                    timer1.Stop();
+                    matilda1.ClearAllImages();
+
+                }
+            }
+            if (b.Name == "rock" && a.Name == "fire!")
+            {
+                b.Dispose();
+                a.Dispose();
+                bullets.Remove(a);
+                breek--;
+                label2.Text = breek.ToString();
+                if (drop == ball_count)
+                {
+
+
+                    label1.Text = "Game Over";
+                    timer1.Stop();
+                    matilda1.ClearAllImages();
+                    timer1.Stop();
+                    matilda1.ClearAllImages();
+                }
+            }
+        }
+
+        private void стартToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+            label2.Text = breek.ToString();
+
+            label1.Text = ""; //Чтобы убрать надпись Game Over, когда начинаем игру не в первый раз
+            for (int i = 0; i < ball_count; i++)
+            {
+                SuperBall ball;
+                ball = new SuperBall(matilda1, "3_метеорит2.png", ClientRectangle.Width / 2 - 25, ClientRectangle.Height / 2 - 25, 50, 50, "ball");
+                ball.topstep = matilda1.RandomInt(-30, 30);
+                ball.leftstep = matilda1.RandomInt(-30, 30);
+                balls.Add(ball);
+            }
+
+            desk = matilda1.CreateSuperImage(null, ClientRectangle.Width / 2 - 50, ClientRectangle.Height - 30 - 30, 100, 30, "desk");
+            desk.OnPaint += PaintDesk;
+
+            int brick_top = 33;
+            for (int i = 0; i < 3; i++)
+            {
+                int brick_left = 35;
+                for (int j = 0; j < 16; /*именно столько помещается*/ j++)
+                {
+                    brick = matilda1.CreateSuperImage(null, brick_left, brick_top, 100, 45, "rock");
+                    brick.OnPaint += Brick_OnPaint;
+                    brick_left += 100 + 20;
+                }
+                brick_top += 45 + 33;
+            }
+        }
+
+        public void настройкаToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            fOptions options;
+            options = new fOptions();
+            options.ShowDialog();
+            if (options.ok==true)
+            {
+                ball_count= options.sar;
+            }
+        }
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (bullet_count > 0)
+            {
+                SuperImage fball = matilda1.CreateSuperImage("3_fireball.png", desk.Left + desk.Width / 2 - 30 / 2, desk.Top - 30, 30, 30, "fire!");
+                bullets.Add(fball);
+                bullet_count--;
+            }
+            //Тут ты забыл обратно возвращать цвет дощечки после того, как закончатся пули
+            else
+            {
+                color = false;
+            }
+        }
+
+        private void Form1_MouseEnter(object sender, EventArgs e)
+        {
+            Cursor.Hide();
+        }
+
+        private void Form1_MouseLeave(object sender, EventArgs e)
+        {
+            Cursor.Show();
+        }
+    }
+}
+ 
